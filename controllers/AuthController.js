91a229c6b6abb232f8524cd52f2ab123775447a4
Reginaldo C Domingos <rcdomingos@live.com>
2024-11-10 20:51:46 -0300

    add auth and session

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -17,17 +17,52 @@ class AuthController {
     res.render("auth/register");
   }
 
-  static registrerUser(req, res) {
-   const { name, email, password, passwordConfirm } = req.body;
+  static async registrerUser(req, res) {
+    const { name, email, password, passwordConfirm } = req.body;
 
-   // password match validation
-   if (password !== passwordConfirm) {
-     req.flash("message", "Senhas não conferem, tente novamente!");
-     res.render("auth/register");
-     return;
-   }
+    // password match validation
+    if (password !== passwordConfirm) {
+      req.flash("message", "Senhas não conferem, tente novamente!");
+      res.render("auth/register");
+      return;
+    }
+
+    //validar se o usuario ja existe
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+    if (checkIfUserExists) {
+      req.flash("message", "Email ja cadastrado, tente novamente!");
+      res.render("auth/register");
+      return;
+    }
+
+    // criptografar a senha
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    // criar o usuario
+    const user = {
+      name,
+      email,
+      password: hash,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+      req.flash("message", "Cadastrado com sucesso!");
+
+      // inicializar a sessao
+      req.session.userId = createdUser.id;
+      // salvar a sessao
+      req.session.save(() => {
+        res.redirect("/toughts");
+      });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      req.flash("message", "Erro ao criar usuário, tente novamente!");
+      res.render("auth/register");
+      return;
+    }
   }
 }
-
 
 module.exports = AuthController;
