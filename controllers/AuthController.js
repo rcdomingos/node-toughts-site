120296f63d6476fa706e5ae7ed0265d84dec027e
Reginaldo C Domingos <rcdomingos@live.com>
2024-11-10 20:10:26 -0300

    add registrer page and css

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,11 @@
 class AuthController {
+  /**
+   * @function login
+   * @description Renderiza a página de login
+   * @param {Request} req - Request da página
+   * @param {Response} res - Response da página
+   */
+
   static login(req, res) {
     res.render("auth/login");
   }
